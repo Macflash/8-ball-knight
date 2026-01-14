3e2571bcedcd8ad707a64a0593fabd2ba0da33a1
Macflash <the.alex.breyfogle@gmail.com>
2026-01-13 22:29:00 -0800

    update to fix bug

--- src/game/move.ts
+++ src/game/move.ts
@@ -1,17 +1,38 @@
 import React from "react";
-import { isMoving, move } from "../physics/ball";
+import { collide, isMoving, move } from "../physics/ball";
 import { GameState } from "./game";
+import { playBallHit } from "../sounds/audio";
 
 export function anythingMoving(game: GameState): boolean {
   return isMoving(game.hero) || game.monsters.some(isMoving);
 }
 
 function updateGame(game: GameState): GameState {
-  move(game.hero);
-  game.table.slowBall(game.hero);
-  game.table.bounceBall(game.hero);
+  const { hero, table, monsters } = game;
 
-  // TODO: support monsters, collisions, etc.
+  // Move the hero
+  move(hero);
+
+  // Move the monsters and check for collisions with the hero
+  for (const monster of monsters) {
+    move(monster);
+    const hit = collide(hero, monster);
+    if (hit) playBallHit();
+  }
+
+  // Check for collisions between the monsters
+  for (let i = 0; i < monsters.length; i++) {
+    for (let j = i + 1; j < monsters.length; j++) {
+      const hit = collide(monsters[i], monsters[j]);
+      if (hit) playBallHit();
+    }
+  }
+
+  // Bound and slow the hero and monsters (and anything else moving!!)
+  table.bounceBall(hero);
+  table.slowBall(hero);
+  monsters.forEach((m) => table.bounceBall(m));
+  monsters.forEach((m) => table.slowBall(m));
 
   return { ...game };
 }
